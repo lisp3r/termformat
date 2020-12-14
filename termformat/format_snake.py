d66ccaf0427d56e531dcd077abf1914cbe1212f5
nowaday <nowaday@github.com>
2020-12-14 22:56:58 +0300

    Update work with ANSI codes

--- termformat/format_snake.py
+++ termformat/format_snake.py
@@ -19,10 +19,12 @@ def rgb_color(color: str) -> tuple:
         raise ValueError(f'RGB color strings must have 3 or 6 digits')
 
 class Formatter():
-    ESCAPE = '\033'
-    RESET = f'{ESCAPE}[0m'
-    BG = '48;2;{r};{g};{b}'
-    FG = '38;2;{r};{g};{b}'
+    # ESCAPE = '\033'
+    RESET = '\033[0m'
+    BG = '\033[48;2;{r};{g};{b}m'
+    FG = '\033[38;2;{r};{g};{b}m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
 
     def __rgb(func):
         """ Formatter's inhouse decorator.
@@ -36,46 +38,42 @@ class Formatter():
 
     def __call__(self, text: str):
         self.__raw_text = text
-        self.__baking_string = f'{self.ESCAPE}['
+        self.__baking_string = ''
         return self
 
     @__rgb
     def fg(self, color):
         """ set foreground color
         """
-        # if isinstance(color, str):
-        #     color = rgb_color(color)
         result = self.FG.format(r=color[0], g=color[1], b=color[2])
-        self.__baking_string += f';{result}'
+        self.__baking_string += result
         return self
 
     @__rgb
     def bg(self, color):
         """ set background color
         """
-        # if isinstance(color, str):
-        #     color = rgb_color(color)
         result = self.BG.format(r=color[0], g=color[1], b=color[2])
-        self.__baking_string += f';{result}'
+        self.__baking_string += result
         return self
 
     def bold(self):
         """ set style to bold
         """
-        self.__baking_string += ';1'
+        self.__baking_string += self.BOLD
         return self
 
     def dim(self):
         """ set style to dim
         """
-        self.__baking_string += ';2'
+        self.__baking_string += self.DIM
         return self
 
     def flush(self):
-        self.__baking_string = f'{self.ESCAPE}['
+        self.__baking_string = ''
         # self.__raw_text = ''
 
     def bake(self) -> str:
-        result = f'{self.__baking_string}m{self.__raw_text}{self.RESET}'
+        result = f'{self.__baking_string}{self.__raw_text}{self.RESET}'
         self.flush()
         return result
--- termformat/tests/tests.py
+++ termformat/tests/tests.py
@@ -14,47 +14,47 @@ class TestFormatter(unittest.TestCase):
     def testBg(self):
         color = '#FAA500'
         string = self.formatter.bg(color).bake()
-        result = check_output(['echo', string]).decode('ascii')
-        expected = f'\x1b[;48;2;250;165;0m{self.text}\x1b[0m\n'
+        result = check_output(['echo', string])
+        expected = str.encode(f'\x1b[48;2;250;165;0m{self.text}\x1b[0m\n')
         self.assertEqual(result, expected)
 
     def testFg(self):
         color = '#FAA500'
         string = self.formatter.fg(color).bake()
-        result = check_output(['echo', string]).decode('ascii')
-        expected = f'\x1b[;38;2;250;165;0m{self.text}\x1b[0m\n'
+        result = check_output(['echo', string])
+        expected = str.encode(f'\x1b[38;2;250;165;0m{self.text}\x1b[0m\n')
         self.assertEqual(result, expected)
 
     def testBold(self):
         string = self.formatter.bold().bake()
-        result = check_output(['echo', string]).decode('ascii')
-        expected = f'\x1b[;1m{self.text}\x1b[0m\n'
+        result = check_output(['echo', string])
+        expected = str.encode(f'\x1b[1m{self.text}\x1b[0m\n')
         self.assertEqual(result, expected)
 
     def testDim(self):
         string = self.formatter.dim().bake()
-        result = check_output(['echo', string]).decode('ascii')
-        expected = f'\x1b[;2m{self.text}\x1b[0m\n'
+        result = check_output(['echo', string])
+        expected = str.encode(f'\x1b[2m{self.text}\x1b[0m\n')
         self.assertEqual(result, expected)
 
     def testFlush1(self):
         self.formatter.bg('#FAA500').bake()
         string = self.formatter.fg('#FAA500').bake()
-        result = check_output(['echo', string]).decode('ascii')
-        expected = f'\x1b[;38;2;250;165;0m{self.text}\x1b[0m\n'
+        result = check_output(['echo', string])
+        expected = str.encode(f'\x1b[38;2;250;165;0m{self.text}\x1b[0m\n')
         self.assertEqual(result, expected)
 
     def testFlush2(self):
         self.formatter.bg('#FAA500').bake()
         string = self.formatter('Another string').fg('#FAA500').bake()
-        result = check_output(['echo', string]).decode('ascii')
-        expected = '\x1b[;38;2;250;165;0mAnother string\x1b[0m\n'
+        result = check_output(['echo', string])
+        expected = str.encode('\x1b[38;2;250;165;0mAnother string\x1b[0m\n')
         self.assertEqual(result, expected)
 
     def testFormatChain(self):
         string = self.formatter('Chain is cool!').bg('#9dc1e3').bold().fg('#1b8148').bake()
-        result = check_output(['echo', string]).decode('ascii')
-        expected = '\x1b[;48;2;157;193;227;1;38;2;27;129;72mChain is cool!\x1b[0m\n'
+        result = check_output(['echo', string])
+        expected = b'\x1b[48;2;157;193;227m\x1b[1m\x1b[38;2;27;129;72mChain is cool!\x1b[0m\n'
         self.assertEqual(result, expected)
 
 
