9a4d26369e8526c9be541eaf245b59fb3f8386e5
nowaday <nowaday@github.com>
2020-12-14 20:19:27 +0300

    Save working setup.py. Trying to make imports right

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from os import path, system
 from sys import exit, executable
-from setuptools import setup, Command
+from setuptools import setup
 from shutil import rmtree
 
 
@@ -16,44 +16,6 @@ try:
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        system(f'{executable} setup.py sdist bdist_wheel --universal')
-
-        self.status('Uploading the package to PyPI via Twine…')
-        system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        system('git tag v{0}'.format(about['__version__']))
-        system('git push --tags')
-
-        exit()
-
-
-
 with open('LICENSE') as f:
     license = f.read()
 
@@ -65,10 +27,6 @@ setup(
     long_description_content_type='text/markdown',
     author='lisp3r',
     author_email='lisp3r@github.com',
-    url='',
-    packages=['termformat', 'tests'],
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    url='github.com/lisp3r/termformat.git',
+    packages=['termformat', 'tests']
 )
-
